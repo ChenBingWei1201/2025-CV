30a658c914849efbd832dcf3dbd4549e93bcc16c
ChenBingWei1201 <rain4030633@gmail.com>
2025-03-22 21:51:06 +0800

    finish part2

--- HW1/hw1_material/part2/main.py
+++ HW1/hw1_material/part2/main.py
@@ -16,7 +16,48 @@ def main():
     img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
     ### TODO ###
+    # read setting file
+    sigma_s = 0
+    sigma_r = 0.0
+    rgb_coefficients = []
 
+    with open(args.setting_path, 'r') as f:
+        for line in f:
+            parts = line.strip().split(',')
+            if len(parts) == 3 and parts[0] != 'R':
+                rgb_coefficients.append(list(map(float, parts)))
+            elif len(parts) == 4:
+                sigma_s = float(parts[1])
+                sigma_r = float(parts[3])
+    # plot grayscale images
+    img_grays = [img_gray]
+    for i in range(len(rgb_coefficients)):
+        coeff = rgb_coefficients[i]
+        img_gray_transformed = (img_rgb[:, :, 0] * coeff[0] + 
+                                img_rgb[:, :, 1] * coeff[1] + 
+                                img_rgb[:, :, 2] * coeff[2]).astype(np.uint8)
+        img_grays.append(img_gray_transformed)
+
+    for i in range(len(img_grays)):
+        cv2.imwrite(f'./output/1/1_gray_{i}.png', img_grays[i])
+
+    # create an instance of Joint_bilateral_filter
+    jbf = Joint_bilateral_filter(sigma_s, sigma_r)
+
+    # apply bilateral filter
+    bf_out = jbf.joint_bilateral_filter(img_rgb, img_rgb).astype(np.uint8)
+    costs = []
+
+    # calculate the cost
+    for i in range(len(img_grays)):
+        jbf_out = jbf.joint_bilateral_filter(img_rgb, img_grays[i]).astype(np.uint8)
+        cv2.imwrite(f'./output/1/1_rgb_{i}.png',  cv2.cvtColor(jbf_out,cv2.COLOR_RGB2BGR))
+        cost = np.sum(np.abs(jbf_out.astype(np.int32)-bf_out.astype(np.int32)))
+        costs.append(cost)
+
+    print(f"Costs: {costs}")
+    print(f"Highest cost: {max(costs)} and index: {costs.index(max(costs))}")
+    print(f"Lowest cost: {min(costs)} and index: {costs.index(min(costs))}")
 
 if __name__ == '__main__':
     main()
